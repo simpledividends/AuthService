2f8d5a6eaa5371f8988eceefa181eccd8cf6e1a4
Emiliy Feldman <feldlime@yandex.ru>
2021-06-13 22:23:04 +0300

    added mail service

---
+++ auth_service/mail/config.py
@@ -0,0 +1,7 @@
+# FIXME: Make pretty subjects, senders, text and html templates
+
+REGISTRATION_EMAIL_SUBJECT = "Подтверждение регистрации"
+REGISTRATION_EMAIL_SENDER = "noreply@{domain}"
+REGISTRATION_EMAIL_TEXT_TEMPLATE = """
+Ссылка для подтверждения mail: {link}
+"""
